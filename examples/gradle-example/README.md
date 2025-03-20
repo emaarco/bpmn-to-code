9aca1de2e11c4ee740677357849d17462f3bd1e0
Marco Schaeck <marco.schaeck@outlook.de>
2025-03-20 11:20:12 +0100

    feat: add example-folder with gradle & maven examples

---
+++ examples/gradle-example/README.md
@@ -0,0 +1,45 @@
+# bpmn-to-code Gradle Plugin Example
+
+This module is demonstrating the usage of the **bpmn-to-code Gradle Plugin**.
+
+## ✨ How to Use
+
+To get started, apply the plugin in your build.gradle.kts file:
+
+```kotlin
+plugins {
+    id("io.github.emaarco.bpmn-to-code-gradle") version "{{latestVersion}}"
+}
+```
+
+If the plugin can not be found yet, please make sure,
+that you've also added the gradle plugin repository to your settings.gradle.kts file:
+
+```kotlin
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+```
+
+Next, configure the generation task by setting its parameters.
+This configuration directs the plugin on where to locate your BPMN files,
+where to generate the API code, and which settings to apply (language, package, and process engine).
+Once configured, bpmn-to-code-gradle automatically processes your BPMN models
+and creates convenient, type-safe references for your application.
+
+```kotlin
+import io.github.emaarco.bpmn.adapter.GenerateBpmnModelsTask
+import io.github.emaarco.bpmn.domain.shared.OutputLanguage
+import io.github.emaarco.bpmn.domain.shared.ProcessEngine
+
+tasks.named("generateBpmnModelApi", GenerateBpmnModelsTask::class) {
+    baseDir = projectDir.toString()
+    filePattern = "src/main/resources/**/*.bpmn"
+    outputFolderPath = "$projectDir/src/main/kotlin"
+    packagePath = "de.emaarco.example"
+    outputLanguage = OutputLanguage.KOTLIN
+    processEngine = ProcessEngine.ZEEBE
+}
+```
---
+++ examples/gradle-example/build.gradle.kts
@@ -0,0 +1,53 @@
+import io.github.emaarco.bpmn.adapter.GenerateBpmnModelsTask
+import io.github.emaarco.bpmn.domain.shared.OutputLanguage
+import io.github.emaarco.bpmn.domain.shared.ProcessEngine
+
+plugins {
+    kotlin("jvm") version "1.9.25"
+    id("io.github.emaarco.bpmn-to-code-gradle") version "0.0.3-alpha"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+// Task for Camunda 7 BPMN models
+tasks.register<GenerateBpmnModelsTask>("generateBpmnModelApiForC7") {
+    baseDir = projectDir.toString()
+    filePattern = "src/main/resources/c7/*.bpmn"
+    outputFolderPath = "$projectDir/src/main/kotlin"
+    packagePath = "io.github.emaarco.c7"
+    outputLanguage = OutputLanguage.KOTLIN
+    processEngine = ProcessEngine.CAMUNDA_7
+}
+
+// Task for Zeebe BPMN models
+tasks.register<GenerateBpmnModelsTask>("generateBpmnModelApiForZeebe") {
+    baseDir = projectDir.toString()
+    filePattern = "src/main/resources/c8/*.bpmn"
+    outputFolderPath = "$projectDir/src/main/kotlin"
+    packagePath = "io.github.emaarco.c8"
+    outputLanguage = OutputLanguage.KOTLIN
+    processEngine = ProcessEngine.ZEEBE
+}
+
+// Aggregate task to run both tasks together
+tasks.register("generateBpmnModels") {
+    dependsOn("generateBpmnModelApiForC7", "generateBpmnModelApiForZeebe")
+}
