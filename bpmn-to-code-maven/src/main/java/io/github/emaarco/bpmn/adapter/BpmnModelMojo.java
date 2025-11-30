869941feb901ae77dab0bc8351fd2a29e0f603ca
Marco Schaeck <marco.schaeck@outlook.de>
2025-11-30 19:00:18 +0100

    docs: fix warnings for maven deployment

--- bpmn-to-code-maven/src/main/java/io/github/emaarco/bpmn/adapter/BpmnModelMojo.java
+++ bpmn-to-code-maven/src/main/java/io/github/emaarco/bpmn/adapter/BpmnModelMojo.java
@@ -8,40 +8,81 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+/**
+ * Maven Mojo for generating type-safe API definitions from BPMN process models
+ */
 @Mojo(
-        name = "generate-bpmn-api",
-        defaultPhase = LifecyclePhase.NONE,
-        requiresProject = false
+		name = "generate-bpmn-api",
+		defaultPhase = LifecyclePhase.NONE,
+		requiresProject = false
 )
 public class BpmnModelMojo extends AbstractMojo {
-
-    @Parameter(property = "baseDir", defaultValue = ".")
-    private String baseDir;
-
-    @Parameter(property = "filePattern", defaultValue = "src/main/resources/*.bpmn")
-    private String filePattern;
-
-    @Parameter(property = "outputFolderPath", defaultValue = "src/main/kotlin")
-    private String outputFolderPath;
-
-    @Parameter(property = "packagePath", defaultValue = "de.emaarco.generated")
-    private String packagePath;
-
-    @Parameter(property = "outputLanguage", defaultValue = "KOTLIN")
-    private String outputLanguage;
-
-    @Parameter(property = "processEngine")
-    private String processEngine;
-
-    @Parameter(property = "useVersioning", defaultValue = "false")
-    private Boolean useVersioning;
-
-    @Override
-    public void execute() {
-        CreateProcessApiFilesystemPlugin plugin = new CreateProcessApiFilesystemPlugin();
-        OutputLanguage language = OutputLanguage.valueOf(outputLanguage);
-        ProcessEngine engine = ProcessEngine.valueOf(processEngine);
-        plugin.execute(baseDir, filePattern, outputFolderPath, packagePath, language, engine, useVersioning);
-    }
-
+	
+	/**
+	 * The base directory for the plugin execution.
+	 * Defaults to the current directory.
+	 */
+	@Parameter(property = "baseDir", defaultValue = ".")
+	private String baseDir;
+	
+	/**
+	 * Pattern for locating BPMN files to process.
+	 * Defaults to "src/main/resources/*.bpmn".
+	 */
+	@Parameter(property = "filePattern", defaultValue = "src/main/resources/*.bpmn")
+	private String filePattern;
+	
+	/**
+	 * Output folder path where the generated API code will be written.
+	 * Defaults to "src/main/kotlin".
+	 */
+	@Parameter(property = "outputFolderPath", defaultValue = "src/main/kotlin")
+	private String outputFolderPath;
+	
+	/**
+	 * Package path for the generated API code.
+	 * Defaults to "de.emaarco.generated".
+	 */
+	@Parameter(property = "packagePath", defaultValue = "de.emaarco.generated")
+	private String packagePath;
+	
+	/**
+	 * Output language for code generation.
+	 * Valid values: KOTLIN, JAVA. Defaults to KOTLIN.
+	 */
+	@Parameter(property = "outputLanguage", defaultValue = "KOTLIN")
+	private String outputLanguage;
+	
+	/**
+	 * Target process-engine for the generated API.
+	 * Valid values: CAMUNDA_7, ZEEBE, OPERATON.
+	 */
+	@Parameter(property = "processEngine")
+	private String processEngine;
+	
+	/**
+	 * Enable API versioning for the generated code.
+	 * Defaults to false.
+	 */
+	@Parameter(property = "useVersioning", defaultValue = "false")
+	private Boolean useVersioning;
+	
+	/**
+	 * Default constructor for maven purposes
+	 */
+	@SuppressWarnings("unused")
+	public BpmnModelMojo() {
+	}
+	
+	/**
+	 * Executes the BPMN API generation process
+	 */
+	@Override
+	public void execute() {
+		CreateProcessApiFilesystemPlugin plugin = new CreateProcessApiFilesystemPlugin();
+		OutputLanguage language = OutputLanguage.valueOf(outputLanguage);
+		ProcessEngine engine = ProcessEngine.valueOf(processEngine);
+		plugin.execute(baseDir, filePattern, outputFolderPath, packagePath, language, engine, useVersioning);
+	}
+	
 }
