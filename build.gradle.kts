843fc610ddd4365f09039d722172367e4fa92e0c
Marco Schaeck <marco.schaeck@outlook.de>
2026-04-26 12:47:34 +0200

    Revert "feat: add OWASP Dependency-Check for vulnerability scanning"
    This reverts commit 49bf066f887ffc4d41d2fa6fceda9f132e45ea63.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,21 +4,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.mavenPublish) apply false
-    alias(libs.plugins.dependencyCheck)
-}
-
-dependencyCheck {
-    failBuildOnCVSS = 7.0f
-    suppressionFile = "config/dependency-check-suppressions.xml"
-    formats = listOf("HTML", "JSON")
-    nvd {
-        apiKey = System.getenv("NVD_API_KEY") ?: ""
-    }
-    analyzers {
-        assemblyEnabled = false
-        nodeAuditEnabled = false
-        nodeEnabled = false
-    }
 }
 
 allprojects {
